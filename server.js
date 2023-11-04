003e5567d3e55f28e79c491f4d1d6500ab9f80f8
Nicolas Carmont <nickcarmont8@gmail.com>
2023-11-04 15:53:52 +0000

    Login

--- server.js
+++ server.js
@@ -28,14 +28,72 @@ const app = express();
 // Serve homepage
 app.get('/', (req, res) => {
   res.send(`
-    <html>
-      <head>
-        <title>My App</title>
-      </head>
-      <body>
-        <a href="/auth/google">Log in with Google</a>
-      </body>
-    </html>
+  <html>
+
+  <head>
+    <title>AnthroPals</title>
+    
+    <!-- Use a nice font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+  
+    <!-- Include a CSS framework like Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    
+    <!-- Custom CSS -->
+    <style>
+      body {
+        font-family: 'Poppins', sans-serif;
+        background-color: #f1f1f1; 
+      }
+      
+      .container {
+        max-width: 500px;
+        margin-top: 50px;
+        background-color: #fff;
+        padding: 30px;
+        border-radius: 10px;
+        box-shadow: 0 10px 20px rgba(0,0,0,0.1);
+      }
+      
+      h1 {
+        font-size: 32px;
+        text-align: center; 
+      }
+      
+      .btn-google {
+        background-color: #dd4b39;
+        color: #fff;
+        border: none;
+      }
+      
+      .btn-google img {
+        width: 20px;
+        margin-right: 10px;
+      }
+    </style>
+  
+  </head>
+  
+  <body>
+      <div class="container">
+      
+        <h1>AnthroPals</h1>
+        
+        <h2 class="text-center">Increasing the world's face time</h2>
+        
+        <p class="text-center">
+          AnthroPals pre-fills your social calendar with relevant events for you during the week.
+        </p>
+  
+      <button class="btn btn-block btn-google">
+        <img src="google-logo.png"> Log in with Google
+      </button>
+  
+    </div>
+  
+  </body>
+  
+  </html>
   `);
 });
 
