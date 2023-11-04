f1e020e2570ddd1fe56c7d2d2e26360f6ffaade9
Andy Lee <andylee024@gmail.com>
2023-11-04 16:59:08 +0000

    Add prototype code for brave API

---
+++ search/search_event.py
@@ -0,0 +1,34 @@
+
+
+def find_events_by_prompt():
+    """Given a prompt around user preferences, translate this to search queries"""
+    pass
+
+
+def generate_google_searches_by_prompt():
+    """Given a prompt, generate google searches to find event website"""
+    pass
+
+
+def has_events_page():
+    """Given a website determine if there's a website"""
+    pass
+
+
+def locate_event_link_on_website():
+    """Given a website tree, locate the event link if it exists"""
+    pass
+
+
+def run_events_locator():
+
+    # generate google search
+
+    # get google links
+
+    # go to google link
+
+    # locate event page (if it exists)
+
+    # parse and handle event page
+    pass
